5dee6660c779b29da430a4629b9680187328bfa3
Chris .D <178487367+diotchristopher@users.noreply.github.com>
2024-09-03 18:43:48 +0200

    Add project files.

---
+++ Source/AdvancedObsMechanics/Public/AdvancedObsMechanicsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedObsMechanicsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAdvancedObsMechanicsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdvancedObsMechanicsGameMode();
+};
+
+
+
